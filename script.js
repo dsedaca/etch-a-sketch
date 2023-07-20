2cc9a3e634744aca224be1b716ffff3b0cf37af7
Daniel Sedaca <46464470+dsedaca@users.noreply.github.com>
2023-07-20 16:58:21 -0400

    Added clear button and start the erase button

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let grid = document.getElementById('container');
 const show = document.getElementById('show');
+const black = document.getElementById('black');
+const rainbow = document.getElementById('rainbow');
+const eraser = document.getElementById('eraser');
+const clear = document.getElementById('clear');
 let containerHeight = 500;
 let containerWidth = 500;
 let divGrid;
 let divRow;
 let divRemove;
 let oldBlocks = 0;
-let divHeight;
 
 function createGrid(num) {
     let figureDivHeight = parseInt(containerHeight) / parseInt(num);
@@ -20,7 +23,7 @@ function createGrid(num) {
         for (var j = 0; j < num; j++) {
             divRow = document.createElement('div');
             divRow.className = 'row';
-            divRow.style.backgroundColor = 'white';
+            divRow.style.backgroundColor = 'red';
             newDivHeight = figureDivHeight + 'px';
             newDivWidth = figureDivWidth + 'px';
             divRow.style.height = newDivHeight;
@@ -33,13 +36,25 @@ function createGrid(num) {
     oldBlocks = num;
 }
 
-function removeGrid (oldValue) {
+function removeGrid (num) {
     divRemove = document.getElementById('container').lastChild
 
-    for (let i = oldValue; i > 0; i--) {
+    for (let i = num; i > 0; i--) {
         divRemove.remove();
         divRemove = document.getElementById('container').lastChild
     }
+}
+
+function clearGrid (num) {
+    let totalGrid = document.getElementsByClassName('row');
+    for (let i = 0; i < totalGrid.length; i++) {
+        totalGrid[i].style.backgroundColor = 'white';
+    }
+
+}
+
+function eraseGrid () {
+
 }
 
 show.addEventListener('click', () => {
@@ -54,4 +69,20 @@ show.addEventListener('click', () => {
         createGrid(newBlocks);
     }
     
+});
+
+black.addEventListener('click', () => {
+    
+});
+
+rainbow.addEventListener('click', () => {
+    
+});
+
+eraser.addEventListener('click', () => {
+    document.getElementsByClassName('row').onmouseover
+});
+
+clear.addEventListener('click', () => {
+    clearGrid(oldBlocks); 
 });
